1b90691fb81d55aa358616326c550e0a71835cae
Ilze Koning <ilze.Koning@interfront.co.za>
2019-11-02 21:53:55 +0200

    artwork added

--- source/admin/previews.js
+++ source/admin/previews.js
@@ -65,7 +65,7 @@ const NavigationPreview = ({ entry }) => {
     <header className="header-primary">
       <div className="logo">
         <a href="/">
-          <img src="/images/logo.svg" alt="Kaldi" />
+          <img src="/images/logo.svg" alt="Izzi" />
         </a>
       </div>
       <a className="nav-button">
